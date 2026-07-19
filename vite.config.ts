cc6f6ff184588bcebc41e6341e21c7a232547ab7
Yixuan <y+github@ixuan.ch>
2026-07-19 09:01:29 +0000

    chore: configure base path

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: process.env.BASE_PATH,
   server: {
     host: '0.0.0.0',
     port: 1234
