44fc3db80141d606813cb54a01041a1e38533bb3
Yixuan <56627720+yixuanwu4@users.noreply.github.com>
2026-07-19 15:00:49 +0200

    feat: scaffold pages (#18)

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,8 +1,25 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import HomeView from '../views/HomeView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [],
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: HomeView,
+    },
+    {
+      path: '/quiz/:id',
+      name: 'quiz',
+      component: () => import('../views/QuizView.vue'),
+    },
+    {
+      path: '/quiz/result',
+      name: 'result',
+      component: () => import('../views/ResultView.vue'),
+    }
+  ],
 })
 
 export default router
